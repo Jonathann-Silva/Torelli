790026b751c6449a5abbc76c103a73dd5a437c9b
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2026-06-09 16:14:05 +0000

    Enviei nosso app para o git e mandei ele para a vercel, agora preciso ad

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBTZaJyyb29f0ZBElswfl0y3k1AN0FEDso",
-  authDomain: "studio-3657521221-d612e.firebaseapp.com",
-  projectId: "studio-3657521221-d612e",
-  storageBucket: "studio-3657521221-d612e.firebasestorage.app",
-  messagingSenderId: "178968479488",
-  appId: "1:178968479488:web:9ccedca68620490e79d5e5",
-  vapidKey: "BCP-VGIe2Atp8Ff0doF2P1lcD9KZVsAeCgws_AiDZBnNtVixLszHCCWvIulBFumJ6i5z2bJdjUeZlRJuxo_JZG8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBTZaJyyb29f0ZBElswfl0y3k1AN0FEDso",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-3657521221-d612e.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-3657521221-d612e",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-3657521221-d612e.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "178968479488",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:178968479488:web:9ccedca68620490e79d5e5",
+  vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY || "BCP-VGIe2Atp8Ff0doF2P1lcD9KZVsAeCgws_AiDZBnNtVixLszHCCWvIulBFumJ6i5z2bJdjUeZlRJuxo_JZG8"
 };
