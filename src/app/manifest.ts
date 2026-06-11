1aada4eee7bc4becfd1dcafeaa2a783c909294da
Jonathan Augusto da Silva <jonathan.silva.992.js@gmail.com>
2026-06-11 15:58:46 +0000

    Quero que o icon do app seja esse, esse icone que ficara no celular quan

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
+  const logoUrl = "https://www.dropbox.com/scl/fi/70fwazrji2098g5fwn6de/Logo.jpg?rlkey=jxz0q85l1qo54pnk0wa2huiqm&st=ead76oo8&raw=1";
+
   return {
     name: 'Barber Torelli',
     short_name: 'Barber Torelli',
@@ -11,14 +13,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#ffbf00',
     icons: [
       {
-        src: 'https://picsum.photos/seed/torelli-icon/192/192',
+        src: logoUrl,
         sizes: '192x192',
-        type: 'image/png',
+        type: 'image/jpeg',
       },
       {
-        src: 'https://picsum.photos/seed/torelli-icon/512/512',
+        src: logoUrl,
         sizes: '512x512',
-        type: 'image/png',
+        type: 'image/jpeg',
       },
     ],
   }
